178ca23dfb088276254016c32f9f55a583ae3805
Kelly Miyashiro <kmiyashiro@users.noreply.github.com>
2024-11-29 04:09:13 -0800

    Remove auto setting token

--- src/session.js
+++ src/session.js
@@ -1,19 +1,11 @@
 // session.js
-import { ENV_TOKEN_KEY } from "./constants.js";
-
 let headers = {
   "Client-Platform": "web",
   "User-Agent":
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
 };
 
-const env = process?.env ?? env ?? {};
-
 // Try to load token from environment variable, rather than from session file.
-if (env[ENV_TOKEN_KEY]) {
-  headers["Authorization"] = `Token ${env[ENV_TOKEN_KEY]}`;
-  console.log("Loaded token from environment variable." + env[ENV_TOKEN_KEY]);
-}
 let token = null;
 
 export const setToken = (newToken) => {
