1ba657ba211f0eb4dce2c3636f0085f20c9a97ec
Kelly Miyashiro <kmiyashiro@users.noreply.github.com>
2024-11-29 04:08:06 -0800

    Spoof agent

--- src/session.js
+++ src/session.js
@@ -3,6 +3,8 @@ import { ENV_TOKEN_KEY } from "./constants.js";
 
 let headers = {
   "Client-Platform": "web",
+  "User-Agent":
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
 };
 
 const env = process?.env ?? env ?? {};
